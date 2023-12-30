24a276b1a061e2a2f2eff42e13102ac4add7fe15
Ranimbenkerri <102553705+Ranimbenkerri@users.noreply.github.com>
2023-12-30 17:51:47 +0100

    test

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,15 +1,8 @@
-import image from '../assets/image.png'
+
 const Home = () => {
   return (
     <div>
-      <div className="content">
-        <h1>Avocats expérimentés sont prêts à aider.</h1>
-        <p>Bienvenue chez Avvo, où l'expertise juridique rencontre le service exceptionnel.</p>
-         <CostumLink text={"Trouver un avocat"} />
-      </div>
-      <div>
-        <img src={image} alt="" />
-      </div>
+      git checkout -b ranim
     </div>
   )
 }
