c54c9f2e683b822e3416ffbe7e703aaa680c81d6
Ranimbenkerri <102553705+Ranimbenkerri@users.noreply.github.com>
2023-12-31 17:08:39 +0100

    home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,31 @@
+/* eslint-disable import/no-unresolved */
+import image from "../assets/image.png";
+import CostumLink from "../Components/reusable/CostumLink";
+import React from "react";  
+// import Card from "../Components/card";
+import Carousel from "../Components/carousel";
+
 
 const Home = () => {
   return (
-    <div>
-      git checkout -b ranim
-    </div>
+    <div className="container">
+      
+      <div className="flex items-center justify-between px-20 py-0" >
+        <div className="content">
+          <h1 className="mb-10 text-custom-color">Avocats expérimentés <br />sont prêts à aider.</h1>
+          <p className="mb-16">Bienvenue chez Avvo, où lexpertise juridique <br /> rencontre le service exceptionnel.</p>
+          <CostumLink dist={"/search"} text={"Trouver un avocat"} />
+        </div>
+        <div>
+          <img src={image} alt="" />
+        </div>
+      </div>
+      <div className="carousel p-20">
+         <h3 className="text-center mb-20">Avocats de premier plan près de chez vous.</h3>  
+          <Carousel />
+      </div>
+     
+      </div>
   )
 }
 
