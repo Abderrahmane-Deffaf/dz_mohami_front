b576bb495f681c18fbb7f28a5f4240ac8655deb3
Ranimbenkerri <102553705+Ranimbenkerri@users.noreply.github.com>
2024-01-28 21:22:10 +0100

    first coomit

--- src/Components/carousel.jsx
+++ src/Components/carousel.jsx
@@ -2,122 +2,122 @@ import React, { useState, useEffect } from "react";
 import avocat1 from "../assets/avocat/avocat1.png";
 import Card from "../Components/card";
 import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselPrevious,
-  CarouselNext,
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselPrevious,
+    CarouselNext,
 } from "@/Components/ui/carousel";
 const carousel = () => {
-  return (
-    <div dir="ltr">
-      <Carousel>
-        <CarouselContent>
-          <CarouselItem className="md:basis-1/2 lg:basis-1/3">
-            <Card
-              name={"Brandie Luis"}
-              image={avocat1}
-              location={"batns, Algerie"}
-              categories={
-                "lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum"
-              }
-              numberOfStarts="4.5"
-            />
-          </CarouselItem>
-          <CarouselItem className="md:basis-1/2 lg:basis-1/3">
-            <Card
-              name={"Ranim Benkerri"}
-              image={avocat1}
-              location={"Setif, Algerie"}
-              categories={
-                "lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum"
-              }
-              numberOfStarts="4.5"
-            />
-          </CarouselItem>
-          <CarouselItem className="md:basis-1/2 lg:basis-1/3">
-            <Card
-              name={"Abdrhman deffaf"}
-              image={avocat1}
-              location={"Setif, Algerie"}
-              categories={
-                "lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum"
-              }
-              numberOfStarts="4.5"
-            />
-          </CarouselItem>
-          <CarouselItem className="md:basis-1/2 lg:basis-1/3">
-            <Card
-              name={"Ahmed Gouasmia"}
-              image={avocat1}
-              location={"17 october, Bba, Algereie"}
-              categories={
-                "lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum"
-              }
-              numberOfStarts="4.5"
-            />
-          </CarouselItem>
-          <CarouselItem className="md:basis-1/2 lg:basis-1/3">
-            <Card
-              name={"Abdnour zermat"}
-              image={avocat1}
-              location={"17 october, Bba, Algereie"}
-              categories={
-                "lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum"
-              }
-              numberOfStarts="5"
-            />
-          </CarouselItem>
-          <CarouselItem className="md:basis-1/2 lg:basis-1/3">
-            <Card
-              name={"chiheb hamaz"}
-              image={avocat1}
-              location={"Setif, Algerie"}
-              categories={
-                "lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum"
-              }
-              numberOfStarts="5"
-            />
-          </CarouselItem>
-          <CarouselItem className="md:basis-1/2 lg:basis-1/3">
-            <Card
-              name={"Brandie Luis"}
-              image={avocat1}
-              location={"17 october, Bba, Algereie"}
-              categories={
-                "lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum"
-              }
-              numberOfStarts="5"
-            />
-          </CarouselItem>
-          <CarouselItem className="md:basis-1/2 lg:basis-1/3">
-            <Card
-              name={"Ranim benkerri"}
-              image={avocat1}
-              location={"Setif, Algerie"}
-              categories={
-                "lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum"
-              }
-              numberOfStarts="5"
-            />
-          </CarouselItem>
-          <CarouselItem className="md:basis-1/2 lg:basis-1/3">
-            <Card
-              name={"Brandie Luis"}
-              image={avocat1}
-              location={"17 october, Bba, Algereie"}
-              categories={
-                "lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum"
-              }
-              numberOfStarts="5"
-            />
-          </CarouselItem>
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
-    </div>
-  );
+    return (
+        <div dir="ltr">
+            <Carousel>
+                <CarouselContent>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                        <Card
+                            name={"Brandie Luis"}
+                            image={avocat1}
+                            location={"batns, Algerie"}
+                            categories={
+                                "lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum"
+                            }
+                            numberOfStarts="4.5"
+                        />
+                    </CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                        <Card
+                            name={"Ranim Benkerri"}
+                            image={avocat1}
+                            location={"Setif, Algerie"}
+                            categories={
+                                "lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum"
+                            }
+                            numberOfStarts="4.5"
+                        />
+                    </CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                        <Card
+                            name={"Abdrhman deffaf"}
+                            image={avocat1}
+                            location={"Setif, Algerie"}
+                            categories={
+                                "lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum"
+                            }
+                            numberOfStarts="4.5"
+                        />
+                    </CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                        <Card
+                            name={"Ahmed Gouasmia"}
+                            image={avocat1}
+                            location={"17 october, Bba, Algereie"}
+                            categories={
+                                "lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum"
+                            }
+                            numberOfStarts="4.5"
+                        />
+                    </CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                        <Card
+                            name={"Abdnour zermat"}
+                            image={avocat1}
+                            location={"17 october, Bba, Algereie"}
+                            categories={
+                                "lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum"
+                            }
+                            numberOfStarts="5"
+                        />
+                    </CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                        <Card
+                            name={"chiheb hamaz"}
+                            image={avocat1}
+                            location={"Setif, Algerie"}
+                            categories={
+                                "lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum"
+                            }
+                            numberOfStarts="5"
+                        />
+                    </CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                        <Card
+                            name={"Brandie Luis"}
+                            image={avocat1}
+                            location={"17 october, Bba, Algereie"}
+                            categories={
+                                "lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum"
+                            }
+                            numberOfStarts="5"
+                        />
+                    </CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                        <Card
+                            name={"Ranim benkerri"}
+                            image={avocat1}
+                            location={"Setif, Algerie"}
+                            categories={
+                                "lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum"
+                            }
+                            numberOfStarts="5"
+                        />
+                    </CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                        <Card
+                            name={"Brandie Luis"}
+                            image={avocat1}
+                            location={"17 october, Bba, Algereie"}
+                            categories={
+                                "lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum"
+                            }
+                            numberOfStarts="5"
+                        />
+                    </CarouselItem>
+                </CarouselContent>
+                <CarouselPrevious />
+                <CarouselNext />
+            </Carousel>
+        </div>
+    );
 };
 export default carousel;
 
